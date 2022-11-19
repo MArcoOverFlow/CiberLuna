e22677d50fcc3fea5e779817b0ef099291f56018
MArcoOverFlow <marco.amado.ma88@gmail.com>
2022-11-18 19:11:56 -0600

    Implementa Navigation Drawer

--- app/src/main/java/com/example/ciberluna/InicioActivity.java
+++ app/src/main/java/com/example/ciberluna/InicioActivity.java
@@ -1,14 +1,85 @@
 package com.example.ciberluna;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class InicioActivity extends AppCompatActivity {
+import com.example.ciberluna.Fragment.FragmentAgenda;
+import com.example.ciberluna.Fragment.FragmentDatos;
+import com.example.ciberluna.Fragment.FragmentSolicitud;
+import com.example.ciberluna.Fragment.MainFragment;
+import com.google.android.material.navigation.NavigationView;
 
+public class InicioActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    //variables para cargar el fragment principal
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        //Establecer evento OnCreate al navigationView
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+        //Cargar fragmet principal
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container, new MainFragment());
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if(menuItem.getItemId() == R.id.home){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new MainFragment());
+            fragmentTransaction.commit();
+        }
+        if(menuItem.getItemId() == R.id.Agenda){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new FragmentAgenda());
+            fragmentTransaction.commit();
+        }
+        if(menuItem.getItemId() == R.id.solicitud){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new FragmentSolicitud());
+            fragmentTransaction.commit();
+        }
+        if(menuItem.getItemId() == R.id.datos){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new FragmentDatos());
+            fragmentTransaction.commit();
+        }
+        return false;
     }
 }
